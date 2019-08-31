9ae39cce692865d9770be878ac8ff1d38da49eae
Laura Berge <lauraeberge@gmail.com>
2019-08-31 05:22:51 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -7,8 +7,8 @@ def deal_card
   random
 end
 
-def display_card_total(number)
-  puts "Your cards add up to #{number}"
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
