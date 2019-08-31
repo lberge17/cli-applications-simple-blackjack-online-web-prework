9d8506af33bc0a54e6f2d3e4ac49d677698c8b97
Laura Berge <lauraeberge@gmail.com>
2019-08-31 05:32:57 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,7 +23,7 @@ def end_game(card_total)
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
-def initial_round(card_total)
+def initial_round(card_total, food)
   deal_card
   deal_card
   return display_card_total(card_total)
