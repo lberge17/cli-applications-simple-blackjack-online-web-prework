3f8f8e83f5ffdb46a74db45267f1cbe3e28bcc6d
Laura Berge <lauraeberge@gmail.com>
2019-08-31 05:35:05 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -24,9 +24,9 @@ def end_game(card_total)
 end
 
 def initial_round
-  deal_card = a
-  deal_card
-  return display_card_total()
+  first_card = deal_card
+  second_card = deal_card 
+  return display_card_total(first_card + second_card)
 end
 
 def hit?
